0b889124e26a9dd80f4efea6414f682bd19029de
Speedk1929 <IsaacBLee@hotmail.com>
2024-05-15 15:10:25 -0400

    Second Fix For Bullet Firing at Inconsistent Rates
    Turns out the problem was that when moving left and firing left it would cause the bullet to ram into the player hence deleting it from the game. I fixed this by adding the player to the ignored collisions.

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -45,7 +45,7 @@ public class Bullet : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
-        if (collision.gameObject.tag != "Bullet")
+        if (collision.gameObject.tag != "Bullet" && collision.gameObject.tag != "Player")
         {
 
 
